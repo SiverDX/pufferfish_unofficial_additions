f47cae73b07c5def57414451f749e97567fb0ba4
Kai <kai.zahn@yahoo.de>
2024-03-13 12:41:26 +0100

    try to fix attributes

--- src/main/java/de/cadentem/pufferfish_unofficial_additions/mixin/puffish_skills/JsonParseUtilsMixin.java
+++ src/main/java/de/cadentem/pufferfish_unofficial_additions/mixin/puffish_skills/JsonParseUtilsMixin.java
@@ -1,6 +1,6 @@
 package de.cadentem.pufferfish_unofficial_additions.mixin.puffish_skills;
 
-import de.cadentem.pufferfish_unofficial_additions.PUA;
+import de.cadentem.pufferfish_unofficial_additions.utils.Utils;
 import net.minecraft.resources.ResourceLocation;
 import net.puffish.skillsmod.api.utils.JsonParseUtils;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,13 +8,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(JsonParseUtils.class)
-public class JsonParseUtilsMixin {
-    @ModifyArg(method = "lambda$parseAttribute$12", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Registry;getOptional(Lnet/minecraft/resources/ResourceLocation;)Ljava/util/Optional;"), remap = false)
-    private static ResourceLocation modifyNamespace(final ResourceLocation location) {
-        if (location != null && location.getNamespace().equals(PUA.MODID)) {
-            return new ResourceLocation("additional_attributes", location.getPath());
-        }
-
-        return location;
+public abstract class JsonParseUtilsMixin {
+    @ModifyArg(method = "lambda$parseAttribute$10", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Registry;getOptional(Lnet/minecraft/resources/ResourceLocation;)Ljava/util/Optional;"))
+    private static ResourceLocation pufferfish_unofficial_additions$fixAttribute(final ResourceLocation location) {
+        return Utils.fixAttribute(location);
     }
 }
