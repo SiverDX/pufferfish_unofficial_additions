61043f72c357c18597602d5aaa98d2d821e8b99d
Kai <kai.zahn@yahoo.de>
2024-04-16 14:33:34 +0200

    fix fishing exp source in singleplayer - close #2

--- src/main/java/de/cadentem/pufferfish_unofficial_additions/events/ForgeEvents.java
+++ src/main/java/de/cadentem/pufferfish_unofficial_additions/events/ForgeEvents.java
@@ -3,14 +3,13 @@ package de.cadentem.pufferfish_unofficial_additions.events;
 import de.cadentem.pufferfish_unofficial_additions.experience.FishingExperienceSource;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.ItemFishedEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.puffish.skillsmod.SkillsMod;
 
-@Mod.EventBusSubscriber(Dist.DEDICATED_SERVER)
+@Mod.EventBusSubscriber
 public class ForgeEvents {
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void grantFishingExperience(final ItemFishedEvent event) {
